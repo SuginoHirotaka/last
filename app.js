875db7a6aa4ca99cbcb3c2e61e182e0fe6687f56
SuginoHirotaka <sugino0223@outlook.jp>
2016-01-08 15:09:54 +0900

    基礎工事２

--- app.js
+++ app.js
@@ -1,64 +1,14 @@
-var player = document.querySelector("audio");
-player.addEventListener("play", hello);
-player.addEventListener("pause", goodbye);
+window.onload = function(){
 
-window.addEventListener("load", initialize);
-function initialize(event){
-   pauseButton.hidden = true;
-   volumenControl.value = 1;
-   seekbar.max = player.duration;
-}
+  var output = document.getElementById('output');
 
-var playButton = document.querySelector("#playButton");
-playButton.addEventListener("click", playMusic);
-function playMusic(event){
-   player.play();
-   pauseButton.hidden = false;
-   playButton.hidden = true;
-}
+  window.addEventListener('devicemotion', function(e){
+    gravity = e.accelerationIncludingGravity;
 
-var pauseButton = document.querySelector("#pauseButton");
-pauseButton.addEventListener("click", pauseMusic);
-function pauseMusic(event){
-   player.pause();
-　 pauseButton.hidden = true;
-   playButton.hidden = false;
-}
+    output.innerHTML
+    = 'x方向: '+gravity.x
+    + '<br>y方向: '+gravity.y;
 
+  },true);
 
-function hello(event){
-  console.log("hello");
-  console.log(event);
-}
-
-function goodbye(event){
-   console.log("goddbye");
-}
-
-var skipButton = document.querySelector("#skipButton");
-skipButton.addEventListener("click", skip10sec);
-function skip10sec(event){
-   player.currentTime = player.currentTime + 10;
-}
-
-var backButton = document.querySelector("#backButton");
-backButton.addEventListener("click", back10sec);
-function back10sec(event){
-   player.currentTime = player.currentTime - 10;
-}
-
-var volumenControl = document.querySelector("#volumeControl");
-volumeControl.addEventListener("change", changeVolume);
-function changeVolume(event){
-  player.volume = volumenControl.value;
-}
-
-var seeknbar = document.querySelector("#seekbar");
-seekbar.addEventListener("change", changeseek);
-function changeseek(event){
-  player.currentTime = seekbar.value;
-}
-
-function updateseek(event){
-  seekbar.value = player.currentTime;
-}
+};
